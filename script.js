c467f28ed27e62b1f4135c9512c72d858dad4229
nicrausaz <n.crausaz99@gmail.com>
2016-11-10 15:29:08 +0100

    show db entries working

--- script.js
+++ script.js
@@ -41,26 +41,26 @@ function checkData() {
 function showData() {
     var MongoClient = require('mongodb').MongoClient;
 
-
-
     // Connect to the db
+    var dbErrorDiv = document.getElementById("dbErrors");
+    var dbErrorMsg = "An error occured while trying to connecting to db";
+
     MongoClient.connect("mongodb://localhost:27017/test", function (err, db) {
-        if (err) { return console.dir(err); } //add error msg here
+
+        if (err) { dbErrorDiv.innerHTML = dbErrorMsg }
 
         var collection = db.collection('user');
         var docs = [{ firstname: "Nicolas", name: "Crausaz" }];
 
-        collection.insert(docs, function (err, result) {
 
-            collection.find().toArray(function (err, items) { });
-
-            var stream = collection.find({ firstname: { $ne: 2 } }).stream();
-            console.dir(stream.on("data", function (item) { }));
-            console.dir(stream.on("end", function () { }));
-
-            //collection.findOne({ firstname: "Nicolas" }, function (err, item) { });
-
+        collection.find({ firstname: "Nicolas" }).toArray(function (err, results) {
+            if (results != ""){
+                document.getElementById("displayCollectionDiv").innerHTML = JSON.stringify(results);
+            }else{
+                document.getElementById("dbErrors").innerHTML = "No data were found";
+            }
         });
-    });
 
+        //collection.findOne({ firstname: "Nicolas" }, function (err, item) { });
+    });
 }
