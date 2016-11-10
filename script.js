75600af88630833c3d69428d2fbc310afb215d22
nicrausaz <n.crausaz99@gmail.com>
2016-11-10 19:17:55 +0100

    refactor done

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
+// GLOBAL SCRIPTS
+
+var MongoClient = require('mongodb').MongoClient,
+    test = require('assert');
+// Connection url
+var url = 'mongodb://localhost:27017/test';
+// Connect using MongoClient
+MongoClient.connect(url, function (err, db) {
+    if (err) {
+        var dbErrorDiv = document.getElementById("dbErrors");
+        var dbErrorMsg = "An error occured while trying to connecting to db";
+        dbErrorDiv.innerHTML = dbErrorMsg;
+    } else {
+        console.log("connected to DB");
+        return col = db.collection('user');
+    }
+});
+
+
+
 // SCRIPTS FOR ADDENTRY.HTML
 function sendDatas() {
-    var MongoClient = require('mongodb').MongoClient,
-        test = require('assert');
-
-    // Connection url
-    var url = 'mongodb://localhost:27017/test';
-
-    // Connect using MongoClient
-    MongoClient.connect(url, function (err, db) {
-        console.log("connected to DB");
-
-        // Create a collection
-        var col = db.collection('user');
-
-        //create vars
-        var userFirstname = document.getElementById("firstname").value;
-        var userName = document.getElementById("name").value;
-
-        // Insert data as documents
-        col.insert([{ firstname: userFirstname, name: userName }], function (err, result) {
-            test.equal(null, err);
-        });
+    // Insert data as documents
+    col.insert([{ firstname: getUserFirstname(), name: getUserName() }], function (err, result) {
+        test.equal(null, err);
     });
 }
 
@@ -29,16 +31,35 @@ function printErrorMsg() {
 }
 
 function checkData() {
+
+    //TODO rewrite this function
     var userFirstname = document.getElementById("firstname").value;
     var userName = document.getElementById("name").value;
 
-    (userFirstname == "" || userFirstname === null) ? error1 = 0 : error1 = 1;
-    (userName == "" || userName === null) ? error2 = 0 : error2 = 1;
+    (getUserFirstname() == "" || getUserFirstname() === null) ? error1 = 0 : error1 = 1;
+    (getUserName() == "" || getUserName() === null) ? error2 = 0 : error2 = 1;
     return ((error1 && error2) ? sendDatas() : printErrorMsg());
 }
 
 
 // SCRIPTS FOR SHOWENTRY
+
+function checkIfInputsEmpty() {
+
+    console.log(getSelectedAttribute());
+    console.log(getSelectedExpression());
+
+
+    if ((getSelectedAttribute() == "notSelected") && (getSelectedExpression() == "")) {
+        console.log("all data");
+        showData();
+        
+    } else {
+        console.log("not all data");
+        showDataAccordingToSelection();
+    }
+    //TODO: Check if 2 input are filled
+}
 
 function getSelectedAttribute() {
     return document.getElementById("requestedAttribute").value;
@@ -48,54 +69,42 @@ function getSelectedExpression() {
     return document.getElementById("requestedExpression").value;
 }
 
+function getUserFirstname() {
+    return userFirstname = document.getElementById("firstname").value;
+}
+
+function getUserName() {
+    return userName = document.getElementById("name").value;
+}
+
+function getCollection(db) {
+    return col = db.collection('user');
+}
+
+function checkResults(results) {
+    if (results != "") {
+        document.getElementById("displayCollectionDiv").innerHTML = JSON.stringify(results);
+    } else {
+        document.getElementById("dbErrors").innerHTML = "No data were found";
+    }
+}
+
 function showData() {
-    var MongoClient = require('mongodb').MongoClient;
-
-    // Connect to the db
-    var dbErrorDiv = document.getElementById("dbErrors");
-    var dbErrorMsg = "An error occured while trying to connecting to db";
-
-    MongoClient.connect("mongodb://localhost:27017/test", function (err, db) {
-
-        if (err) { dbErrorDiv.innerHTML = dbErrorMsg }
-
-        var col = db.collection('user');
-
-        //show all data
-        col.find().toArray(function (err, results) {
-            if (results != "") {
-                document.getElementById("displayCollectionDiv").innerHTML = JSON.stringify(results);
-            } else {
-                document.getElementById("dbErrors").innerHTML = "No data were found";
-            }
-        });
+    col.find().toArray(function (err, results) {
+        checkResults(results);
     });
 }
 
 function showDataAccordingToSelection() {
-    console.log("Looking for your request");
-    var MongoClient = require('mongodb').MongoClient;
-
-    var dbErrorDiv = document.getElementById("dbErrors");
-    var dbErrorMsg = "An error occured while trying to connecting to db";
 
     var attribute = getSelectedAttribute();
     var expression = getSelectedExpression();
 
-    console.log(attribute + " " + expression);
+    console.log("Looking for " + expression + " as " + attribute);
 
-    MongoClient.connect("mongodb://localhost:27017/test", function (err, db) {
-
-        if (err) { dbErrorDiv.innerHTML = dbErrorMsg }
-        var col = db.collection('user');
-
-        //show all data
-        col.find({ "firstname": expression }).toArray(function (err, results) {
-            if (results != "") {
-                document.getElementById("displayCollectionDiv").innerHTML = JSON.stringify(results);
-            } else {
-                document.getElementById("dbErrors").innerHTML = "No data were found";
-            }
-        });
+    //Show asked data
+    col.find({ firstname: expression }).toArray(function (err, results) {
+        checkResults(results);
     });
 }
+
